fdd4f1520ac1512bc9c8811953deb5dbc138a6cf
HorigutiStudent <s23c1127lc@s.chibakoudai.jp>
2024-11-29 11:44:53 +0900

    自分で動作テストを終了

---
+++ get_place_info.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+import sys
+from parce_input_data import ParceInputData
+import re
+
+
+class GetPlaceInfo:
+    def __init__(self,):
+        self.parce_input_data = ParceInputData()
+    
+    
+    def done(self) -> str:
+        input_data = self.parce_input_data.get_data()
+        return self.__get_place(input_data)
+    
+    
+    def __get_place(self,input_data:str) -> str:
+        print(input_data)
+        if input_data[0] == "t":#入力の頭文字が津田沼のtなら
+            return "td"
+        elif input_data[0] == "s":#入力の頭文字が新習志野のsなら
+            return self.__get_floor(input_data)
+        else:
+            raise KeyError(f"the input option {input_data} is unexpected")
+    
+    
+    def __get_floor(self,input_data:str) -> str:
+        """新習志野校舎のみ何階かの情報が必要"""
+        m = re.search(r'\d+$',input_data).group()
+        if len(m) != 1:
+            raise KeyError(f"floor info is not match correct pattern {input_data}")
+        if m == "1":
+            return "sd1"
+        else:
+            return "sd2"
---
+++ parce_input_data.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+import sys
+
+class ParceInputData:
+    def __init__(self,):
+        self.input_data = ""
+  
+    def get_data(self) -> str:
+        self.input_data = sys.stdin.read()
+        return self.input_data
+  
+    def to_num(self,type:str="int") -> list:
+        inputs = []
+        for num in self.input_data:
+            if type == "int":
+                inputs.append(int(num))
+            elif type == "float":
+                inputs.append(float(num))
+            else:
+                raise KeyError(f"the type {type} is unexpected")
+        return inputs
